c93bcc4b6f5f91fffe5cb2fb7fac536137405411
Aleksey Profatilov <snakeduse@gmail.com>
2015-09-21 23:22:47 +0300

    Добавил таблицу для карточек и для связей карточки с пользователем

--- db/schema.sql
+++ db/schema.sql
@@ -1,1 +1,8 @@
+-- таблица пользователей
 CREATE TABLE users ( id SERIAL PRIMARY KEY, email VARCHAR(255) NOT NULL, password VARCHAR(255) NOT NULL);
+
+-- таблица с карточками
+CREATE TABLE cards ( id SERIAL PRIMARY KEY, title VARCHAR(255) NOT NULL, created_at TIMESTAMP NOT NULL, updated_at TIMESTAMP);
+
+-- таблица связей пользователя с карточками
+CREATE TABLE cards_users (id SERIAL PRIMARY KEY, card_id INTEGER REFERENCES cards(id) NOT NULL, user_id INTEGER REFERENCES users(id) NOT NULL);
