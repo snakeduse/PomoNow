dc4b8fef9261149025bac2ef6b562549e686b6f6
Aleksey Profatilov <snakeduse@gmail.com>
2015-09-26 00:18:14 +0300

    Добавил создание таблицы для задач

--- db/schema.sql
+++ db/schema.sql
@@ -6,3 +6,12 @@ CREATE TABLE cards ( id SERIAL PRIMARY KEY, title VARCHAR(255) NOT NULL, created
 
 -- таблица связей пользователя с карточками
 CREATE TABLE cards_users (id SERIAL PRIMARY KEY, card_id INTEGER REFERENCES cards(id) NOT NULL, user_id INTEGER REFERENCES users(id) NOT NULL);
+
+-- таблица задач
+CREATE TABLE tasks (
+        id SERIAL PRIMARY KEY,
+        title VARCHAR(255) NOT NULL,
+        is_open BOOLEAN NOT NULL DEFAULT TRUE,
+        count_pomodoros INT NOT NULL DEFAULT 0,
+        created_at TIMESTAMP NOT NULL DEFAULT now(),
+        updated_at TIMESTAMP);
